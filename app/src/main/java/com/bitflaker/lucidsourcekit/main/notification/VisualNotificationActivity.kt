6fb9acb7d4239255daa0a39dadb735411cdc9578
BitFlaker <68316793+BitFlaker@users.noreply.github.com>
2025-01-04 13:50:54 +0100

    Added configurable morphing text
    Added configurable morphing text to the full-screen notification, trying to imitate the behavior of barely readable text in dreams to remind you of performing a reality check

--- app/src/main/java/com/bitflaker/lucidsourcekit/main/notification/VisualNotificationActivity.kt
+++ app/src/main/java/com/bitflaker/lucidsourcekit/main/notification/VisualNotificationActivity.kt
@@ -5,9 +5,14 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.bitflaker.lucidsourcekit.databinding.ActivityVisualNotificationBinding
+import com.bitflaker.lucidsourcekit.utils.Tools
 
 class VisualNotificationActivity : AppCompatActivity() {
     private lateinit var binding: ActivityVisualNotificationBinding
+    private val circleColor = Color.parseColor("#43474E")
+    private val presetMaskCharacters = charArrayOf('⁈', '⁜', '#', 'ᚏ', '⁂', '‽', '⁕', '∞', 'ᚔ')
+    private val stripeAnimationDelay: Long = 256
+    private val circleAnimationDelay: Long = 1152
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,13 +20,24 @@ class VisualNotificationActivity : AppCompatActivity() {
         binding = ActivityVisualNotificationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val color = Color.parseColor("#43474E")
+        // Configure gradient circle
         binding.gcCircle.setMaskColor(Color.BLACK)
-        binding.gcCircle.setColor(color)
+        binding.gcCircle.setColor(circleColor)
         binding.gcCircle.setAnimationDuration(1000)
         binding.gcCircle.setStops(0.6f, 0.005f)
-        binding.gcCircle.startAnimation(1400)
 
-        binding.gsStripe.startAnimations(256)
+        // Configure gradient circle inner text
+        binding.gcCircle.setTextColor(Tools.getAttrColor(com.google.android.material.R.attr.colorOutline, theme))
+        binding.gcCircle.setTextMaskCharacter(presetMaskCharacters[4])
+        binding.gcCircle.setJitterEnabled(true)
+        binding.gcCircle.setMaskJitter(48)
+        binding.gcCircle.setTextJitter(24)
+        binding.gcCircle.setTextSize(Tools.spToPx(this, 20f).toFloat())
+
+        // Start attention grabbing flashing animation
+        binding.gsStripe.startAnimations(stripeAnimationDelay)
+
+        // Start gradient circle animation
+        binding.gcCircle.startAnimation(stripeAnimationDelay + circleAnimationDelay)
     }
 }
--- app/src/main/java/com/bitflaker/lucidsourcekit/views/GradientCircle.kt
+++ app/src/main/java/com/bitflaker/lucidsourcekit/views/GradientCircle.kt
@@ -1,6 +1,7 @@
 package com.bitflaker.lucidsourcekit.views
 
 import android.animation.ValueAnimator
+import android.animation.ValueAnimator.INFINITE
 import android.animation.ValueAnimator.REVERSE
 import android.content.Context
 import android.graphics.Canvas
@@ -9,21 +10,37 @@ import android.graphics.ComposeShader
 import android.graphics.Matrix
 import android.graphics.Paint
 import android.graphics.PorterDuff
+import android.graphics.PorterDuffXfermode
 import android.graphics.RadialGradient
+import android.graphics.Rect
 import android.graphics.Shader
+import android.graphics.Typeface
 import android.util.AttributeSet
 import android.view.View
 import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.LinearInterpolator
 import androidx.annotation.ColorInt
+import androidx.core.animation.doOnEnd
 import com.bitflaker.lucidsourcekit.utils.Tools
+import kotlin.math.abs
+import kotlin.math.max
 import kotlin.math.min
 
 class GradientCircle @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val labelPaint = Paint(Paint.ANTI_ALIAS_FLAG or Paint.DITHER_FLAG).apply {
+        isAntiAlias = true
+        textSize = Tools.spToPx(context, 16f).toFloat()
+        xfermode = PorterDuffXfermode(PorterDuff.Mode.XOR)
+        color = Color.WHITE
+        typeface = Typeface.MONOSPACE
+    }
     private val gradientMatrix = Matrix()
-    private val animator: ValueAnimator
+    private val circleAnimator: ValueAnimator
+    private val textAnimator: ValueAnimator
+    private val textFadeInAnimator: ValueAnimator
     private var startOnMeasured = false
     private var gradientsInitialized = false
 
@@ -52,8 +69,44 @@ class GradientCircle @JvmOverloads constructor(
     private var baseStops = floatArrayOf(0f, 0.7f, 0.74f, 1f)
     private var currentScale = startMaskOffset
 
+    // Text animation related variables
+    private val textBounds: Rect = Rect()
+    private val textOffsetY = labelPaint.textSize * 0f
+    private val textPhrases = Array(3) { "" }
+    private val textDimensions = Array(4) { IntArray(2) { 0 } }
+    private var currentTextSlide = -1f
+    private var textMaskCharacter = "#"
+    private var textMaskCharacters = 0
+    private var textMaskString: String? = null
+    private var textOpacity = 0f
+    private var textJitterEnabled = false
+    private var textAnimationDuration = 4200L
+    private var maskJitter = 64
+    private var textJitter = 24
+
     init {
-        animator = ValueAnimator.ofFloat(startMaskOffset, 1f).apply {
+        setTextPhrases("am I", "Dreaming?", "check!")
+
+        // Setup animations
+        textAnimator = ValueAnimator.ofFloat(-0.25f, 2.25f).apply {
+            duration = textAnimationDuration
+            repeatCount = INFINITE
+            repeatMode = REVERSE
+            interpolator = LinearInterpolator()
+            addUpdateListener { animation ->
+                currentTextSlide = animation.animatedValue as Float
+                invalidate()
+            }
+        }
+        textFadeInAnimator = ValueAnimator.ofFloat(0f, 1f).apply {
+            duration = 1400
+            interpolator = LinearInterpolator()
+            addUpdateListener { animation ->
+                textOpacity = animation.animatedValue as Float
+                invalidate()
+            }
+        }
+        circleAnimator = ValueAnimator.ofFloat(startMaskOffset, 1f).apply {
             duration = 750
             repeatMode = REVERSE
             interpolator = AccelerateDecelerateInterpolator()
@@ -63,12 +116,54 @@ class GradientCircle @JvmOverloads constructor(
                 invalidate()
             }
         }
+        circleAnimator.doOnEnd {
+            textFadeInAnimator.start()
+            textAnimator.start()
+        }
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+
+        // Draw the animated text in case it is visible
+        if (textOpacity > 0f) {
+            labelPaint.alpha = (255 * textOpacity).toInt()
+
+            drawText(canvas, 0)
+            drawText(canvas, 1)
+            drawText(canvas, 2)
+
+            // Draw text mask
+            textMaskString?.let {
+                // Calculate the position of the mask (with optional jitter)
+                val randomJitterX = if (textJitterEnabled) (-maskJitter..maskJitter).random() / 10f else 0f
+                val randomJitterY = if (textJitterEnabled) (-maskJitter..maskJitter).random() / 10f else 0f
+                val startX = (width - textDimensions[textDimensions.lastIndex][0]) / 2f + randomJitterX
+                val startY = (height - textDimensions[textDimensions.lastIndex][1]) / 2f + randomJitterY
+
+                // Draw the mask
+                canvas.drawText(it, startX, startY, labelPaint)
+            }
+        }
+
+        // Draw the animated gradient circle
         canvas.drawCircle(centerX, centerY, radius, paint)
-        // TODO: Add some kind of unreadable or changing text in the center of the circle
+    }
+
+    private fun drawText(canvas: Canvas, index: Int) {
+        // Calculate the position of the text (with optional jitter)
+        val randomJitterX = if (textJitterEnabled) (-textJitter..textJitter).random() / 10f else 0f
+        val randomJitterY = if (textJitterEnabled) (-textJitter..textJitter).random() / 10f else 0f
+        val x = (width - textDimensions[index][0]) / 2f - randomJitterX
+        val y = (height - textDimensions[index][1]) / 2f - textOffsetY + textOffsetY * index - randomJitterY
+
+        // Calculate the alpha for the current text
+        val previousAlpha = labelPaint.alpha
+        labelPaint.alpha = min(255, (max(0f, (1 - abs(currentTextSlide - index) / 3f) - 0.3f) * 255).toInt() + (0..64).random())
+
+        // Draw the text and reset the alpha to its original value
+        canvas.drawText(textPhrases[index], x, y, labelPaint)
+        labelPaint.alpha = previousAlpha
     }
 
     override fun onSizeChanged(width: Int, height: Int, oldWidh: Int, oldHeight: Int) {
@@ -79,8 +174,8 @@ class GradientCircle @JvmOverloads constructor(
         centerY = height / 2f
 
         setupGradient()
-        if (startOnMeasured && !animator.isStarted) {
-            animator.start()
+        if (startOnMeasured && !circleAnimator.isStarted) {
+            circleAnimator.start()
         }
     }
 
@@ -117,6 +212,10 @@ class GradientCircle @JvmOverloads constructor(
         maskGradient.setLocalMatrix(gradientMatrix)
     }
 
+    /*+************************************
+     **   Circle configuration methods   **
+     ************************************+*/
+
     fun setColor(@ColorInt color: Int) {
         colors[2] = color
         colors[3] = color
@@ -142,27 +241,87 @@ class GradientCircle @JvmOverloads constructor(
 
     fun setStartMaskOffset(offset: Float) {
         startMaskOffset = offset
-        animator.setFloatValues(startMaskOffset, 1f)
+        circleAnimator.setFloatValues(startMaskOffset, 1f)
         currentScale = startMaskOffset
     }
 
+    fun setBlink(blink: Boolean) {
+        circleAnimator.repeatCount = if (blink) 1 else 0
+    }
+
     fun setAnimationDuration(duration: Long) {
-        animator.duration = duration
+        circleAnimator.duration = duration
     }
 
     fun startAnimation(startDelay: Long = 0) {
         startOnMeasured = true
-        animator.startDelay = startDelay
+        circleAnimator.startDelay = startDelay
         if (gradientsInitialized) {
-            animator.start()
+            circleAnimator.start()
         }
     }
 
-    fun setBlink(blink: Boolean) {
-        animator.repeatCount = if (blink) 1 else 0
+    fun stopAnimation() {
+        circleAnimator.cancel()
     }
 
-    fun stopAnimation() {
-        animator.cancel()
+    /*+**********************************
+     **   Text configuration methods   **
+     **********************************+*/
+
+    fun setTextPhrases(topText: String?, centerText: String?, bottomText: String?) {
+        setTextPhrase(0, topText)
+        setTextPhrase(1, centerText)
+        setTextPhrase(2, bottomText)
+
+        val longestString = textPhrases.maxBy { it.length }
+
+        // Setup the text mask
+        textMaskCharacters = longestString.length
+        textMaskString = textMaskString ?: textMaskCharacter.repeat(textMaskCharacters)
+
+        // Calculate the mask string dimensions
+        labelPaint.getTextBounds(textMaskString, 0, textMaskString!!.length, textBounds)
+        textDimensions[3][0] = textBounds.width()
+        textDimensions[3][1] = textBounds.centerY()
+    }
+
+    private fun setTextPhrase(index: Int, text: String?) {
+        textPhrases[index] = text ?: textPhrases[index]
+        labelPaint.getTextBounds(textPhrases[index], 0, textPhrases[index].length, textBounds)
+        textDimensions[index][0] = textBounds.width()
+        textDimensions[index][1] = textBounds.centerY()
+    }
+
+    fun setTextMaskCharacter(maskCharacter: Char) {
+        textMaskCharacter = maskCharacter.toString()
+        textMaskString = textMaskCharacter.repeat(textMaskCharacters)
+    }
+
+    fun setTextSize(textSize: Float) {
+        labelPaint.textSize = textSize
+
+        // Recalculate all text measurements without changing text
+        setTextPhrases(null, null, null)
+    }
+
+    fun setTextColor(@ColorInt color: Int) {
+        labelPaint.color = color
+    }
+
+    fun setJitterEnabled(enabled: Boolean) {
+        textJitterEnabled = enabled
+    }
+
+    fun setMaskJitter(jitter: Int) {
+        maskJitter = jitter
+    }
+
+    fun setTextJitter(jitter: Int) {
+        textJitter = jitter
+    }
+
+    fun setTextAnimationDuration(duration: Long) {
+        textAnimationDuration = duration
     }
 }
