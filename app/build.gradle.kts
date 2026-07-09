b187c2bf3777172252e8045aff8cea058c1b8beb
BitFlaker <68316793+BitFlaker@users.noreply.github.com>
2026-07-09 15:49:21 +0000

    Release version 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.bitflaker.lucidsourcekit"
         minSdk = 24
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
